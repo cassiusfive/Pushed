7fc41e40ef67ca5189ac62a085277ad4f579f502
Cassius Villareal <cassiusfive@gmail.com>
2025-02-10 20:39:05 -0800

    Initial Commit

---
+++ Pushed/PushedApp.swift
@@ -0,0 +1,32 @@
+//
+//  PushedApp.swift
+//  Pushed
+//
+//  Created by Cassius Villareal on 2/10/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct PushedApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
